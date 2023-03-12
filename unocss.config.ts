4159ffd9097e3cc2cd93d3d20020f384e166390f
Hime-Hina <1545340072@qq.com>
2023-03-12 15:38:35 +0800

    Fixed: The list marker of `ul` and `ol` overflows.

--- unocss.config.ts
+++ unocss.config.ts
@@ -16,7 +16,13 @@ export default defineConfig({
       scale: 1.1,
       cdn: 'https://esm.sh/',
     }),
-    presetTypography(),
+    presetTypography({
+      cssExtend: {
+        "ul,ol": {
+          "list-style-position": "inside;"
+        }
+      }
+    }),
   ],
   transformers: [transformerVariantGroup(), transformerDirectives()],
   shortcuts: [{
@@ -43,6 +49,6 @@ export default defineConfig({
     'gen-text-wrapper': 'my-4 fc gap-2 transition-opacity',
     'gen-textarea': 'w-full px-3 py-3 min-h-12 max-h-36 rounded-sm bg-(slate op-15) resize-none base-focus placeholder:op-50 dark:(placeholder:op-30) scroll-pa-8px',
     'sys-edit-btn': 'inline-fcc gap-1 text-sm bg-slate/20 px-2 py-1 rounded-md transition-colors cursor-pointer hover:bg-slate/50',
-  }]
+  }],
 })
 
