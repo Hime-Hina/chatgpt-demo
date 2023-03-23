9428f729e8bdd04d1e43e13bd4475fa69922c72d
Hime-Hina <1545340072@qq.com>
2023-03-23 20:52:32 +0800

    refactor: adjust the position of the msg-copy btn.

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -35,13 +35,9 @@ export default ({ role, message }: Props) => {
     const rawCode = fence(...args)
 
     return `<div class="parent" relative>
-    <div data-code=${encodeURIComponent(
-      token.content,
-    )} class="copy-btn gpt-copy-btn group parent-hover:op-90">
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="currentColor" d="M28 10v18H10V10h18m0-2H10a2 2 0 0 0-2 2v18a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2Z" /><path fill="currentColor" d="M4 18H2V4a2 2 0 0 1 2-2h14v2H4Z" /></svg>
-          <div class="group-hover:op-100 gpt-copy-tips">
-            ${copied() ? '已复制！' : '复制'}
-          </div>
+    <div data-code=${encodeURIComponent(token.content)} class="copy-btn gpt-copy-btn group op-0 parent-hover:op-90">
+      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="currentColor" d="M28 10v18H10V10h18m0-2H10a2 2 0 0 0-2 2v18a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2Z" /><path fill="currentColor" d="M4 18H2V4a2 2 0 0 1 2-2h14v2H4Z" /></svg>
+      <div class="gpt-copy-tips group-hover:op-100">${copied() ? '已复制！' : '复制'}</div>
     </div>
     ${rawCode}
     </div>`
@@ -63,27 +59,24 @@ export default ({ role, message }: Props) => {
     }
   })
 
+  const content = typeof message === 'function' ? message() : (typeof message === 'string' ? message : '')
   const htmlString = () => {
-    const msg = typeof message === 'function' ? message() : (typeof message === 'string' ? message : '')
-    const result = md.render(msg)
-
-    return `<div class="parent" relative>
-    <div data-code=${encodeURIComponent(
-      msg,
-    )} class="copy-btn gpt-copy-btn group parent-hover:op-90">
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="currentColor" d="M28 10v18H10V10h18m0-2H10a2 2 0 0 0-2 2v18a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2Z" /><path fill="currentColor" d="M4 18H2V4a2 2 0 0 1 2-2h14v2H4Z" /></svg>
-          <div class="group-hover:op-100 gpt-copy-tips">
-            ${copied() ? '已复制！' : '复制'}
-          </div>
-    </div>
-    ${result}
-    </div>`
+    const result = md.render(content)
+    return `${result}`
   }
 
   return (
     <div class="px-4 py-2 transition-colors -mx-4 md:hover:bg-slate/3">
       <div class="flex gap-3 rounded-lg" class:op-75={role === 'user'}>
-        <div class={`shrink-0 w-7 h-7 mt-4 rounded-full op-80 ${roleClass[role]}`} />
+        <div class="flex flex-col items-center">
+          <div class={`shrink-0 w-7 h-7 mt-4 rounded-full op-80 ${roleClass[role]}`} />
+          <div data-code={encodeURIComponent(content)} class="copy-btn group my-2 gpt-copy-btn" style={{ position: 'initial' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="currentColor" d="M28 10v18H10V10h18m0-2H10a2 2 0 0 0-2 2v18a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V10a2 2 0 0 0-2-2Z" /><path fill="currentColor" d="M4 18H2V4a2 2 0 0 1 2-2h14v2H4Z" /></svg>
+            <div class="relative" style={{ top: '-1rem', right: '1.8rem' }}>
+              <div class="gpt-copy-tips group-hover:op-100">{copied() ? '已复制！' : '复制'}</div>
+            </div>
+          </div>
+        </div>
         <div class="message min-w-0 flex-1 break-words prose" innerHTML={htmlString()} />
       </div>
     </div>
